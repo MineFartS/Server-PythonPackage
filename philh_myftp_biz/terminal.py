b8970bbd93f459163f8d1307def88e04aa353c4d
Phil Hunt <philiphuntiv2@gmail.com>
2026-01-28 09:29:34 -0500

    Moved Logging Stream Handler to __init__

--- philh_myftp_biz/terminal.py
+++ philh_myftp_biz/terminal.py
@@ -379,94 +379,13 @@ class ParsedArgs:
 class Log:
 
     #========================================================
-    # VERBOSE
-
-    from sys import argv as __argv
-
-    VERBOSE = (len({'-v', '--verbose'} & set(__argv)) >= 1)
-
-    if VERBOSE:
-        LEVEL = 10
-    else:
-        LEVEL = 20
-
-    #========================================================
     # WRITERS
-
-    from logging import basicConfig, StreamHandler
 
     from logging import debug    as VERB
     from logging import info     as INFO
     from logging import warning  as WARN
     from logging import error    as FAIL
     from logging import critical as CRIT
-
-    class CustomStreamHandler(StreamHandler):
-        from logging import Formatter
-        
-        class CustomFormatter(Formatter):
-
-            def format(self, r:'LogRecord'):
-                from traceback import print_exception
-                from io import StringIO
-                from .db import Color
-                from .time import now
-
-                # If the record is from a different module
-                if r.name != 'root':
-                    # Do Nothing
-                    return ''
-                
-                # If the record is either from this module or the main execution
-                else:
-
-                    Traceback = StringIO()
-
-                    # Get the current time as an OBJ
-                    n = now()
-
-                    if r.exc_info:
-
-                        print_exception(*r.exc_info, file=Traceback)
-
-                    # Parse the Terminal color value and the level name from the record
-                    match r.levelno:
-
-                        case 10: COLOR, LEVEL = ('WHITE',   'VERB')
-                        
-                        case 20: COLOR, LEVEL = ('WHITE',   'INFO')
-                        
-                        case 30: COLOR, LEVEL = ('YELLOW',  'WARN')
-                        
-                        case 40: COLOR, LEVEL = ('RED',     'FAIL')
-                        
-                        case 50: COLOR, LEVEL = ('MAGENTA', 'CRIT')
-
-                    # Return a string to be printed to the terminal
-                    return \
-                        f'\n{Color.values[COLOR]}\033[1m'+ \
-                        f'{n.year-2000:02d}-{n.month:02d}-{n.day:02d} {n.hour:02d}-{n.minute:02d}-{n.second:02d}.{n.centisecond:02d} '+ \
-                        f'{r.filename}:{r.lineno} '+ \
-                        f'{LEVEL}\033[22m '+ \
-                        f'{r.msg}\033[0m\n'+ \
-                        Traceback.getvalue()
-        
-        def __init__(self):
-
-            super().__init__(stdout)
-
-            # Allow all messages
-            self.setLevel(10)
-
-            self.setFormatter(self.CustomFormatter())
-            
-            # No New Line
-            self.terminator = ''
-
-    basicConfig(
-        level = LEVEL,
-        handlers = [CustomStreamHandler()]
-    )
 
     #========================================================
 
