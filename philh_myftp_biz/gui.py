512bd43ba9fead231fa04340aaba06a72763c696
Phil Hunt <philiphuntiv2@gmail.com>
2026-03-04 15:55:05 -0500

    Update gui.py

--- philh_myftp_biz/gui.py
+++ philh_myftp_biz/gui.py
@@ -16,15 +16,36 @@ class GUI:
         self._tk.title(value)
 
     #====================================================
+    # SIZE
+
+    @property
+    def size(self) -> tuple[int, int]:
+
+        height = self._tk.winfo_height()
+        width = self._tk.winfo_width()
+
+        return (width, height)
+
+    @size.setter
+    def size(self, value:tuple[int, int]) -> None:
+
+        self._tk.geometry(f'{value[0]}x{value[1]}')
+
+    #====================================================
 
     def __init__(self) -> None:
         from tkinter import Tk
 
         self._tk = Tk()
-        
-        self.title = 'New Window'
+
+        # Prevent User from resizing/maximizing window
+        self._tk.resizable(width=False, height=False)
 
         self.run = self._tk.mainloop
+
+        self.title = 'New Window'
+
+        self.size = (500, 300)
 
     def start(self) -> None:
         from .process import Thread
@@ -38,7 +59,20 @@ class GUI:
 
         Label(
             master = self._tk, 
-            text = text
+            text = text,
+            pady = 10
+        ).pack()
+
+    def Header(self,
+        text: str = '<Header>'
+    ) -> None:
+        from tkinter import Label
+
+        Label(
+            master = self._tk,
+            text = text,
+            font = ('TkDefaultFont', 20),
+            pady = 15
         ).pack()
 
     def Button(self,
@@ -50,5 +84,6 @@ class GUI:
         Button(
             master = self._tk, 
             text = text, 
-            command = func
+            command = func,
+            pady = 5
         ).pack()
