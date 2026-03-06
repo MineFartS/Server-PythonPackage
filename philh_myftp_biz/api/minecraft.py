aa81b09f60bb5826fffc5f19b23d4a10dcd829fc
Phil Hunt <philiphuntiv2@gmail.com>
2026-03-06 16:04:25 -0500

    Update minecraft.py

--- philh_myftp_biz/api/minecraft.py
+++ philh_myftp_biz/api/minecraft.py
@@ -29,3 +29,49 @@ class ModrinthMod:
     @cached_property
     def url(self) -> str:
         return self._data['files'][0]['url']
+
+class FabricMC:
+
+    @cached_property
+    def serverURL(self) -> str:
+        """URL of Server jarfile"""
+        return f'https://meta.fabricmc.net/v2/versions/loader/{self.gameV}/{self.loaderV}/{self.installerV}/server/jar'
+
+    @cached_property
+    def gameV(self) -> str: # pyright: ignore[reportReturnType]
+        """Game Version"""
+        from ..web import get
+
+        request = get('https://meta.fabricmc.net/v2/versions/game')
+
+        for item in request.json():
+
+            if item['stable']:
+
+                return item['version']
+            
+    @cached_property
+    def loaderV(self) -> str: # pyright: ignore[reportReturnType]
+        """Loader Version"""
+        from ..web import get
+
+        request = get(f'https://meta.fabricmc.net/v1/versions/loader/{self.gameV}')
+
+        for item in request.json():
+
+            if item['loader']['stable']:
+
+                return item['loader']['version']
+
+    @cached_property
+    def installerV(self) -> str: # pyright: ignore[reportReturnType]
+        """Installer Version"""
+        from ..web import get
+
+        request = get(f'https://meta.fabricmc.net/v2/versions/installer')
+
+        for item in request.json():
+
+            if item['stable']:
+
+                return item['version']
