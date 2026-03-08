70801e03487962cfe9aaff889bf1575abc9a50a1
Phil Hunt <philiphuntiv2@gmail.com>
2026-03-08 13:47:01 -0400

    Update VirusTotal.py

--- philh_myftp_biz/api/VirusTotal.py
+++ philh_myftp_biz/api/VirusTotal.py
@@ -2,7 +2,52 @@ from functools import cached_property
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
+    from vt.object import Object as VTobj
     from ..pc import Path
+
+class VirusReport:
+
+    def __init__(self,
+        file: Path,
+        VTobj: 'VTobj'
+    ) -> None:
+
+        self.file = file
+
+        self._VTobj = VTobj
+
+        self.results: dict = VTobj.last_analysis_results
+    
+    @cached_property
+    def warnings(self) -> list[dict]:
+
+        _warnings = []
+
+        for data in self.results.values():
+
+            if data['result'] != None:
+            
+                _warnings += {data}
+
+        return _warnings
+
+    @cached_property
+    def score(self):
+        """
+        Safety Score
+        
+        ```
+        0: Unsafe
+        ...
+        1: Safe
+        ```
+        """
+
+        total = len(self.results)
+        
+        undetected = (total - len(self.warnings))
+
+        return (undetected / total)
 
 class VirusTotal:
 
@@ -18,13 +63,19 @@ class VirusTotal:
 
         def scan(self,
             file: 'Path'
-        ):
+        ) -> VirusReport:
             """Retrieve the analysis report for a file"""
 
             # TODO
         
             # Check for existing report first
-            file_obj = self.client.get_object(f"/files/{file.hash}")
+            obj = self.client.get_object(f"/files/{file.hash}")
 
-            return file_obj.last_analysis_stats, file_obj.permalink
+            return VirusReport(file, obj)
+
+            #return file_obj.last_analysis_stats, file_obj.permalink
+        
+            # with open("path/to/file.exe", "rb") as f:
+            #     analysis = client.scan_file(f)
+            #     print(analysis)
 
