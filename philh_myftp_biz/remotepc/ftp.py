96934dc23e488973fbc3a4040dafef171c80e1d9
Phil Hunt <philiphuntiv2@gmail.com>
2026-03-13 21:22:51 -0400

    Update ftp.py

--- philh_myftp_biz/remotepc/ftp.py
+++ philh_myftp_biz/remotepc/ftp.py
@@ -14,6 +14,8 @@ class FTPPath:
     def __str__(self) -> str:
         return self.path
 
+    __repr__ = __str__
+
     def download(self,
         local: 'Path'
     ) -> None:
@@ -26,6 +28,100 @@ class FTPPath:
                 f"RETR {self.path}",
                 stream.write
             )
+
+    @cached_property
+    def cd(self) -> _cd:
+        return _cd(self.ftp, self)
+
+    @property
+    def is_dir(self) -> bool:
+        from ftplib import error_perm
+
+        try:
+            self.cd.open()
+            self.cd.back()
+            return True
+        except error_perm:
+            return False
+    
+    @property
+    def is_file(self) -> bool:
+        return (not self.is_dir)
+    
+    def child(self, name:str) -> "FTPPath":
+        
+        if self.is_file:
+            raise TypeError("Parent path cannot be a file")
+
+        return FTPPath(self.path.rstrip('/') + '/' + name)
+
+    @cached_property
+    def children(self) -> Generator[FTPPath]:
+
+        with self.cd:
+        
+            for name in self.ftp._client.nlst():
+
+                yield FTPPath(
+                    ftp = self.ftp,
+                    path = (self.path.rstrip('/') + '/' + name)
+                )
+
+    @cached_property
+    def descendants(self) -> Generator[FTPPath]:
+
+        def _scan(path:FTPPath):
+    
+            for _path in path.children:
+
+                yield _path
+            
+                if _path.is_dir:
+                    yield from _scan(_path)
+
+        yield from _scan(self)
+
+    def seg(self,
+        i: int = -1
+    ) -> str:
+        
+        if self.path[-1] == '/':
+            path = self.path[:-1]
+        else:
+            path = self.path
+        
+        return path.split(sep='/')[i]
+
+    @property
+    def ext(self) -> str|None:
+
+        seg: str = self.seg()
+
+        if '.' in seg:
+
+            return seg[seg.rfind('.')+1:].lower()
+        
+    @cached_property
+    def name(self) -> str:
+
+        name = self.seg()
+
+        # Check if file has ext
+        if self.ext:
+            # Return name without ext
+            return name[:name.rfind('.')]
+
+        else:
+            # Return filename
+            return name
+
+    @property
+    def size(self) -> int:
+
+        if self.is_file:
+            return self.ftp._client.size(self.path) # pyright: ignore[reportReturnType]
+        else:
+            raise TypeError("Cannot get size of a folder")
 
 @dataclass
 class FTP:
@@ -54,15 +150,45 @@ class FTP:
             passwd = self.password
         )
 
+        ftp.encoding = 'latin-1'
+
         return ftp
     
-    def cd(self, path:str) -> None:
-        self._client.cwd(path)
+    def cd(self,
+        path: FTPPath
+    ) -> None:
+        self._client.cwd(str(path))
 
-    @cached_property
-    def children(self) -> Generator[FTPPath]:
-        
-        for path in self._client.nlst():
+    @property
+    def cwd(self) -> FTPPath:
+        return FTPPath(self, self._client.pwd())
 
-            yield FTPPath(self, path)
+    def Path(self, path:str) -> FTPPath:
+        return FTPPath(self, path)
 
+@dataclass
+class _cd:
+
+    ftp: FTP
+    path: FTPPath
+
+    __via_with = False
+    
+    def __enter__(self) -> None:
+        self.__via_with = True
+        self.open()
+
+    def __exit__(self, *_) -> None:
+        if self.__via_with:
+            self.back()
+
+    def open(self) -> None:
+        """Change CWD to the given path"""
+
+        self._back = self.ftp.cwd
+
+        self.ftp.cd(self.path)
+
+    def back(self) -> None:
+        """Change CWD to the previous path"""
+        self.ftp.cd(self._back)
