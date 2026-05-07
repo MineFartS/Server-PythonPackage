b094a03fa64e10f4e42421f994c96baa2d3b1bde
Phil Hunt <philiphuntiv2@gmail.com>
2026-05-07 11:57:30 -0400

    Update install.bat

--- install.bat
+++ install.bat
@@ -2,4 +2,12 @@
 
 cd /d "%~dp0"
 
+:: Install/Update Package
 pip install --upgrade .
+
+:: Locate package directory using pip
+for /f "delims=" %%i in ('pip show philh_myftp_biz ^| findstr "Location:"') do set "DIR=%%i"
+Set "DIR=%DIR:~10%\philh_myftp_biz"
+
+:: Precompile package
+python -m compileall -f "%DIR%"
