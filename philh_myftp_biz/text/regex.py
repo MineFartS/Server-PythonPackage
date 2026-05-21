a750e53ddbe16b3888534fa0d2d6feda12ffd973
Phil Hunt <philiphuntiv2@gmail.com>
2026-05-21 15:47:19 -0400

    Update regex.py

--- philh_myftp_biz/text/regex.py
+++ philh_myftp_biz/text/regex.py
@@ -8,9 +8,23 @@ class Pattern:
     def __call__(self, string:SupportsStr):
         from re import findall
 
+        _string = str(string)
+
         items = []
  
         for pattern in self.patterns:
-            items += findall(pattern, str(string))
+            items += findall(pattern, _string)
     
         return tuple(items)
+    
+    @staticmethod
+    def from_wildcard(*strings):
+        from fnmatch import translate
+        
+        patterns = []
+
+        for wc in strings:
+            patterns += [translate(wc)]
+        
+        return Pattern(*patterns)
+
