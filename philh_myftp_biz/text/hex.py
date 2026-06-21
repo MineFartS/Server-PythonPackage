237980480f7881c29bb1f9aeb5744caf76cfb1f9
Phil Hunt <philiphuntiv2@gmail.com>
2026-06-21 09:47:00 -0400

    Update hex.py

--- philh_myftp_biz/text/hex.py
+++ philh_myftp_biz/text/hex.py
@@ -1,25 +1,18 @@
 """Wrapper for hexadecimal via dill"""
 
 def valid(string:str) -> bool:
+    from _pickle import UnpicklingError
     """Check if string is a valid dill hexadecimal dump"""
 
     try:
         decode(string)
         return True
-    except (EOFError, ValueError):
+    except EOFError, ValueError, UnpicklingError:
         return False
 
 def decode(value:str):
-    """
-    Convert hexadecimal string back into original value
-
-    Trims input by ';' before processing
-    Ex: 'abc;defg;hij' -> 'defg'
-    """
+    """Convert hexadecimal string back into original value"""
     from dill import loads
-
-    if ';' in value:
-        value = value.split(';')[1]
 
     return loads(bytes.fromhex(value))
 
