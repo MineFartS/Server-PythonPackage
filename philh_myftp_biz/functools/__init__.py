e48546d84b2c67ee06843dddf61d2acdd26ffb27
Phil Hunt <philiphuntiv2@gmail.com>
2026-07-28 17:22:06 -0400

    Update __init__.py

--- philh_myftp_biz/functools/__init__.py
+++ philh_myftp_biz/functools/__init__.py
@@ -89,7 +89,14 @@ def copy_attrs(
         if force or not hasattr(dst, name):
             setattr(dst, name, value)
 
-class cached_property(_cached_property):
+class cached_property[T](_cached_property[T]):
+
+    def __init__(self, func):
+
+        if not callable(func):
+            func = lambda s: func
+
+        super().__init__(func)
 
     def __new__(cls, *_, **__) -> _cached_property:
         return super().__new__(cls)
@@ -99,6 +106,9 @@ class cached_property(_cached_property):
 
     def __delete__(self, inst) -> None:
         inst.__dict__.pop(self.attrname, None)
+    
+    def setter(self, fset):
+        return type(self)(self.func, fset=fset)
 
 def loc(obj:Any) -> str:
     """Get the hexadecimal location of an instance in memory"""
